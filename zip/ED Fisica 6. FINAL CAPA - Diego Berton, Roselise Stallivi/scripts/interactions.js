dfc8b79ebc4ce407ea676abcabe7695986106905
Jorge Madson <jorgemadsonsv@gmail.com>
2022-06-14 23:16:11 -0300

    Interação pag 35

--- zip/ED Fisica 6. FINAL CAPA - Diego Berton, Roselise Stallivi/scripts/interactions.js
+++ zip/ED Fisica 6. FINAL CAPA - Diego Berton, Roselise Stallivi/scripts/interactions.js
@@ -1,15 +1,35 @@
-//Mostrar/Ocultar Sugestão de filme
-document.getElementById("sugestao").onclick = () =>
-  mudarVisibilidade(
-    document.getElementById("bloco-sugestao"),
-    document.getElementById("instrucao")
-  );
 function mudarVisibilidade(bloco, instrucao) {
   if (bloco.style.visibility === "hidden") {
     bloco.style.visibility = "visible";
-    instrucao.style.visibility = "hidden";
+    instrucao ? (instrucao.style.visibility = "hidden") : "";
   } else {
     bloco.style.visibility = "hidden";
-    instrucao.style.visibility = "visible";
+    instrucao ? (instrucao.style.visibility = "visible") : "";
   }
 }
+
+//Dividir os arquivos
+//Mostrar/Ocultar Sugestão de filme
+//part2-0-brincadeiras.html
+document.getElementById("sugestao")
+  ? (document.getElementById("sugestao").onclick = () =>
+      mudarVisibilidade(
+        document.getElementById("bloco-sugestao"),
+        document.getElementById("instrucao")
+      ))
+  : "";
+
+//Quadra de queimada
+//part2-1-esportes
+document.getElementById("quadra1").onclick = () =>
+  mudarVisibilidade(document.getElementById("instrucaoQuadra1"));
+document.getElementById("quadra2").onclick = () =>
+  mudarVisibilidade(document.getElementById("instrucaoQuadra2"));
+document.getElementById("quadra3").onclick = () =>
+  mudarVisibilidade(document.getElementById("instrucaoQuadra3"));
+document.getElementById("quadra4").onclick = () =>
+  mudarVisibilidade(document.getElementById("instrucaoQuadra4"));
+document.getElementById("quadra5").onclick = () =>
+  mudarVisibilidade(document.getElementById("instrucaoQuadra5"));
+document.getElementById("quadra6").onclick = () =>
+  mudarVisibilidade(document.getElementById("instrucaoQuadra6"));
