54ae4adcf3d7fd8571ae5c1e571c8acf6106920a
Jorge Madson <filholouco@gmail.com>
2022-06-20 22:28:01 -0300

    pag 59

---
+++ zip/ED Fisica 6. FINAL CAPA - Diego Berton, Roselise Stallivi/scripts/part2-3.js
@@ -0,0 +1,8 @@
+//part2-3-dancas
+if (typeof mudarVisibilidade !== "undefined") {
+  document.getElementById("sugestao-59").onclick = () =>
+    mudarVisibilidade(
+      document.getElementById("bloco-sugestao-59"),
+      document.getElementById("instrucao-59")
+    );
+}
