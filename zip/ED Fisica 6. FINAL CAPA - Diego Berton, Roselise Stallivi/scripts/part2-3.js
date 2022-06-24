019593a8372b717106a20a37d3c77af740ddf58d
Jorge Madson <filholouco@gmail.com>
2022-06-24 03:05:05 -0300

    pag 67

--- zip/ED Fisica 6. FINAL CAPA - Diego Berton, Roselise Stallivi/scripts/part2-3.js
+++ zip/ED Fisica 6. FINAL CAPA - Diego Berton, Roselise Stallivi/scripts/part2-3.js
@@ -38,4 +38,25 @@ if (typeof mudarVisibilidade !== "undefined") {
       document.getElementById("bloco-sugestao-65-4"),
       document.getElementById("instrucao-65-4")
     );
+
+    document.getElementById("sugestao-67-1").onclick = () =>
+    mudarVisibilidade(
+      document.getElementById("bloco-sugestao-67-1"),
+      document.getElementById("instrucao-67-1")
+    );
+  document.getElementById("sugestao-67-2").onclick = () =>
+    mudarVisibilidade(
+      document.getElementById("bloco-sugestao-67-2"),
+      document.getElementById("instrucao-67-2")
+    );
+  document.getElementById("sugestao-67-3").onclick = () =>
+    mudarVisibilidade(
+      document.getElementById("bloco-sugestao-67-3"),
+      document.getElementById("instrucao-67-3")
+    );
+  document.getElementById("sugestao-67-4").onclick = () =>
+    mudarVisibilidade(
+      document.getElementById("bloco-sugestao-67-4"),
+      document.getElementById("instrucao-67-4")
+    );
 }
